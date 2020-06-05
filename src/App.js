6448bbe03415b1200944473a969dfadd5325b919
neb <10528790@my.uvu.edu>
2020-06-04 22:13:47 -0600

    added layout component and auxiliary hoc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './assets/wc3_logo.png';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          WC3
-        </h1>
-        
-      </header>
+      <Layout>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            WC3
+          </h1>
+        </header>
+      </Layout>
     </div>
   );
 }
