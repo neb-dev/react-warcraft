dfd2ddedb99c326b6c8d6c37a0e8f8897f08872a
neb <10528790@my.uvu.edu>
2020-06-30 23:42:23 -0600

    added property to units array

--- src/assets/Units.js
+++ src/assets/Units.js
@@ -4,90 +4,103 @@ export const UNITS = [
     hitPoints: 220,
     gold: 75,
     food: 1,
-    attack: 5.5
+    attack: 5.5,
+    race: 'Human'
   },
   {
     title: 'Footman',
     hitPoints: 420,
     gold: 135,
     food: 2,
-    attack: 12.5 
+    attack: 12.5,
+    race: 'Human'
   },
   {
     title: 'Knight',
     hitPoints: 835,
     gold: 245,
     food: 4,
-    attack: 34
+    attack: 34,
+    race: 'Human'
   },
   {
     title: 'Rifleman',
     hitPoints: 505,
     gold: 205,
     food: 3,
-    attack: 21 
+    attack: 21,
+    race: 'Human'
   },
   {
     title: 'Motar Team',
     hitPoints: 450,
     gold: 145,
     food: 2,
-    attack: 58 
+    attack: 58,
+    race: 'Human'
   },
   {
     title: 'Gyrocopter',
     hitPoints: 350,
     gold: 220,
     food: 2,
-    attack: 20
+    attack: 20,
+    race: 'Human'
   },
   {
     title: 'Flying Machine',
     hitPoints: 250,
     gold: 100,
     food: 1,
-    attack: 18.5
+    attack: 18.5,
+    race: 'Human'
   },
   {
     title: 'Gryphon Rider',
     hitPoints: 825,
     gold: 280,
     food: 4,
-    attack: 50
+    attack: 50,
+    race: 'Human'
   },
   {
     title: 'Priest',
     hitPoints: 300,
     gold: 135,
     food: 2,
-    attack: 8.5
+    attack: 8.5,
+    race: 'Human'
   },
   {
     title: 'Sorceress',
     hitPoints: 325,
     gold: 155,
     food: 2,
-    attack: 11
+    attack: 11,
+    race: 'Human'
   },
   {
     title: 'Siege Engine',
     hitPoints: 700,
     gold: 195,
     food: 4,
-    attack: 50
+    attack: 50,
+    race: 'Human'
   },
   {
     title: 'Spell Breaker',
     hitPoints: 600,
     gold: 215,
     food: 3,
-    attack: 14
+    attack: 14,
+    race: 'Human'
   },
   {
     title: 'Dragon Hawk',
     hitPoints: 575,
     gold: 200,
     food: 3,
-    attack: 19
+    attack: 19,
+    race: 'Human'
   }
 ];
