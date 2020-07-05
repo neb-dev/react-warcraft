5cc0519882aa38beeb6122cc12a2288a16042933
neb <10528790@my.uvu.edu>
2020-07-05 16:03:09 -0600

    added orc units

--- src/assets/Units.js
+++ src/assets/Units.js
@@ -105,6 +105,138 @@ export const UNITS = [
   },
   {
     title: 'Peon',
+    hitPoints: 250,
+    gold: 75,
+    food: 1,
+    attack: 7.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Grunt',
+    hitPoints: 700,
+    gold: 200,
+    food: 3,
+    attack: 19.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Headhunter',
+    hitPoints: 350,
+    gold: 135,
+    food: 2,
+    attack: 25,
+    race: 'Orc'
+  },
+  {
+    title: 'Berserker',
+    hitPoints: 450,
+    gold: 135,
+    food: 2,
+    attack: 25,
+    race: 'Orc'
+  },
+  {
+    title: 'Demolisher',
+    hitPoints: 425,
+    gold: 220,
+    food: 4,
+    attack: 80.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Raider',
+    hitPoints: 610,
+    gold: 180,
+    food: 3,
+    attack: 25,
+    race: 'Orc'
+  },
+  {
+    title: 'Tauren',
+    hitPoints: 1300,
+    gold: 280,
+    food: 5,
+    attack: 33,
+    race: 'Orc'
+  },
+  {
+    title: 'Shaman',
+    hitPoints: 335,
+    gold: 130,
+    food: 2,
+    attack: 8.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Witch Doctor',
+    hitPoints: 315,
+    gold: 145,
+    food: 2,
+    attack: 12,
+    race: 'Orc'
+  },
+  {
+    title: 'Spirit Walker',
+    hitPoints: 500,
+    gold: 195,
+    food: 3,
+    attack: 19.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Kodo Beast',
+    hitPoints: 1000,
+    gold: 255,
+    food: 1,
+    attack: 18,
+    race: 'Orc'
+  },
+  {
+    title: 'Wind Rider',
+    hitPoints: 570,
+    gold: 265,
+    food: 4,
+    attack: 40,
+    race: 'Orc'
+  },
+  {
+    title: 'Troll Batrider',
+    hitPoints: 325,
+    gold: 165,
+    food: 2,
+    attack: 14,
+    race: 'Orc'
+  },
+  {
+    title: 'Spirit Wolf',
+    hitPoints: 200,
+    gold: 0,
+    food: 0,
+    attack: 11.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Dire Wolf',
+    hitPoints: 300,
+    gold: 0,
+    food: 0,
+    attack: 16.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Shadow Wolf',
+    hitPoints: 500,
+    gold: 0,
+    food: 0,
+    attack: 21.5,
+    race: 'Orc'
+  },
+  {
+    title: 'Serpent Ward',
+    hitPoints: 75,
+    gold: 0,
+    food: 0,
+    attack: 12,
     race: 'Orc'
   }
 ];
