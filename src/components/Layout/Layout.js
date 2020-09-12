0f6bcc73f91d96fc7d7e889e0173ee7e9a98fafe
neb <10528790@my.uvu.edu>
2020-09-11 22:52:00 -0600

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,9 +4,12 @@ import Auxiliary from '../../hoc/Auxiliary';
 import logo from '../../assets/wc3_logo.png';
 import './Layout.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => (
   <Auxiliary>
     <div className="Layout">
+      <Toolbar />
       <header className="Layout-header">
         <img src={logo} className="Layout-logo" alt="logo" />
       </header>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Toolbar.css';
+
+const toolbar = (props) => (
+    <div className="Toolbar">
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </div>
+);
+
+export default toolbar;
