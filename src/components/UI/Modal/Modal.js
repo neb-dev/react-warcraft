0c5cdc16f0c16f3be360335020b1fc1a63ab5ab4
neb <10528790@my.uvu.edu>
2020-09-18 22:34:48 -0600

    added modal code and styling

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './Modal.css';
+
 const Modal = (props) => (
-  <div></div>
+  <div className="Modal">
+    { props.children }
+  </div>
 );
 
 export default Modal;
