1b4d19875fe989675cc150e4b997172d32f8d0fd
neb-dev <10528790@my.uvu.edu>
2020-09-25 21:34:01 -0600

    added unit stats via props

--- src/components/Units/Unit/Unit.js
+++ src/components/Units/Unit/Unit.js
@@ -5,7 +5,12 @@ import './Unit.css';
 const Unit = (props) => {
   return (
     <div className="Unit">
-      
+      <div>{ props.title }</div>
+      <div>{ props.hitPoints }</div>
+      <div>{ props.gold }</div>
+      <div>{ props.food }</div>
+      <div>{ props.attack }</div>
+      <div>{ props.race }</div>
     </div>
   );
 };
